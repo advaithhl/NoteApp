e02bee51641bd55af9791691fed703eddad08d4e
Advaith HL <niceadvaith@gmail.com>
2019-10-18 20:39:26 +0530

    Initial commit

---
+++ app/src/main/java/com/advaith/project_advaith/activities/ResumeActivity.java
@@ -0,0 +1,16 @@
+package com.advaith.project_advaith.activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.advaith.project_advaith.R;
+
+public class ResumeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resume);
+    }
+}
